d6987715189d77e76d5c937abfcfbae6063675e1
Jeff Kelley <SlaunchaMan@gmail.com>
2013-08-17 13:51:40 -0400

    Initial Commit

---
+++ StringsSort/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  StringsSort
+//
+//  Created by Jeff Kelley on 8/17/13.
+//  Copyright (c) 2013 Detroit Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
